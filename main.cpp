c8cc2a5f4f1295e92b30021a256698c816c7add8
EduardaB <eduardacbianquine@gmail.com>
2020-05-08 01:09:05 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    string s;
+    int c, r, e;
+
+    cout << "Sexo: ";
+    cin >> s;
+    cout << "Quantidade de cervejas: ";
+    cin >> c;
+    cout << "Quantidade de refrigerantes: ";
+    cin >> r;
+    cout << "Quantidade de espetinhos: ";
+    cin >> e;
+
     return 0;
 }
